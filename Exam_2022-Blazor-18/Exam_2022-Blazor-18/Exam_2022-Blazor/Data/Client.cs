9a95c13c0b1565009ad9ddc618eb8e843be86988
Даниль Фахрутдинов <danilfahrutdinov357@mail.ru>
2022-07-01 22:10:17 +0300

    added 17 and 18

---
+++ Exam_2022-Blazor-18/Exam_2022-Blazor-18/Exam_2022-Blazor/Data/Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exam_2022_Blazor.Data
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Mail { get; set; }
+        public static List<Client> st = new List<Client>();
+
+        public Client(string name, string login, string mail)
+        {
+            this.Name = name;
+            this.Login = login;
+            this.Mail = mail;
+        }
+        public Client()
+        {
+            
+        }
+        public static void AddClient(string name, string login, string mail)
+        {
+            Client std = new Client(name, login, mail);
+            st.Add(std);
+        }
+        public static List<Client> GetClient()
+        {
+            return st;
+        }
+    }
+}
